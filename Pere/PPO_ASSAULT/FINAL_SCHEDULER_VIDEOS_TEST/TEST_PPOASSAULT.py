87cdc70d5dbe9c239ddd52896ef559668cc125ce
virtual.machine <a.gasconmarzo@gmailcom>
2024-12-15 13:28:35 +0100

    a

--- Pere/PPO_ASSAULT/FINAL_SCHEDULER_VIDEOS_TEST/TEST_PPOASSAULT.py
+++ Pere/PPO_ASSAULT/FINAL_SCHEDULER_VIDEOS_TEST/TEST_PPOASSAULT.py
@@ -13,28 +13,35 @@ from stable_baselines3.common.env_util import make_atari_env
 from stable_baselines3.common.vec_env import VecVideoRecorder
 from stable_baselines3 import PPO
 
+#set the video folder and how many steps to take.
 video_folder = 'videos'
 video_length = 6000
 
+#generate the environment for the trained model.
 env = make_atari_env('Assault-v4', n_envs=1, seed=0)
 env = VecFrameStack(env, n_stack=4)
 
+#reset the environment
+#used VecVideoRecorder to record the steps taken and get a video. Set x == 0, to record from the beggining to desired lenght.
 env.reset()
 env = VecVideoRecorder(env, video_folder,
                        record_video_trigger=lambda x: x == 0, video_length=video_length,
                        name_prefix=f"testtrained-")
 
+#load the best performing model.
 model_path = "/fhome/pmlai01/ReinforcementLearning_Project_Group1-5/Pere/PPO_ASSAULT/DEFINITIVE_SCHEDULER/ASSAULT_PPOlong2048.zip"
 model = PPO.load(model_path, env=env)
 
+#generate first observation 
 obs = env.reset()
-
+#take the desired number of steps and use the model's trained policy to take actions.
 for _ in range(6000):
   action, _ = model.predict(obs, deterministic=True)
   obs, _, done, info = env.step(action)
 
 env.close()
 
+#reset the environemt and do the same but with an untrained model.
 env = make_atari_env('Assault-v4', n_envs=1, seed=0)
 env = VecFrameStack(env, n_stack=4)
 
